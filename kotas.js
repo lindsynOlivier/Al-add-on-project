5ef593449cf4dc5a23fd020829cd54806d2491cc
lindsynOlivier <lindsayolivier119@gmail.com>
2025-02-05 16:26:04 +0200

    Create the underWorld.js

---
+++ kotas.js
@@ -0,0 +1,27 @@
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "c5fb501012566e9bd47a3bo39e25tb37";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert, and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Do not add a title. Make sure to follow the user instructions. Sign the poem with Shecodes AI at the end, NOT the beginning.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generate">Please wait .... generating a poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
